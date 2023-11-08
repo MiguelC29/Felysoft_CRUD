aba5117931c3f51e6af76638588d2db44e0a45aa
MiguelC29 <miguelito28c@gmail.com>
2023-11-08 15:47:42 -0500

    Restructurando orden de carpetas y añadiendo archivo php con la conexion a la base de datos

---
+++ FELYSOFT/CONTROLADOR/Conexion.php
@@ -0,0 +1,9 @@
+<?php
+    //Conectamos con el servidor
+    $host = "localhost";
+    $user = "root";
+    $clave = "";
+    $bd = "db_felysoft";
+
+    $conectar = mysqli_connect($host, $user, $clave, $bd);
+?>
