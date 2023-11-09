7ed3348b0e437659638bcfcc54a9b789663113fc
MiguelC29 <miguelito28c@gmail.com>
2023-11-08 21:35:25 -0500

    Creacion formulario para registro de productos

--- FELYSOFT/VISTA/principal/scriptP.js
+++ FELYSOFT/VISTA/principal/scriptP.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const agregarProductosBtn = document.getElementById('agregarProductosBtn');
     const formularioAgregarProductos = document.getElementById('formularioAgregarProductos');
     const cerrarFormulario = document.getElementById('cerrarFormulario');
+    const closeForm = document.getElementById('closeForm');
+    
+    const newProductbtn = document.getElementById('addNewProduct');
+    const formAddProduct = document.getElementById('formAddProduct');
+
     const overlay = document.createElement('div');
     overlay.className = 'overlay';
   
@@ -14,5 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
       document.body.removeChild(overlay);
       formularioAgregarProductos.style.display = 'none';
     });
+
+    newProductbtn.addEventListener('click', function () {
+      formularioAgregarProductos.style.display = 'none';
+      document.body.appendChild(overlay);
+      formAddProduct.style.display = 'block';
+    });
+
+    closeForm.addEventListener('click', function () {
+      document.body.removeChild(overlay);
+      formAddProduct.style.display = 'none';
+    });
+
   });
   
