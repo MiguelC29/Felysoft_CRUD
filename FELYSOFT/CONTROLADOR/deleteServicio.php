e31c91da75692c24f8d7f312725539464e5843bb
devsan-g3 <santimahe001@gmail.com>
2023-11-08 22:49:15 -0600

    new cambio delete

--- FELYSOFT/CONTROLADOR/deleteServicio.php
+++ FELYSOFT/CONTROLADOR/deleteServicio.php
@@ -8,10 +8,9 @@ $delete="DELETE FROM tiposervicio WHERE idTipoServicio = $idServicio";
 $query=mysqli_query($conectar, $delete);
 
 if ($query){
-    echo "<script>alert('Registro eliminado correctamente.'); location.href='../VISTA/principal/servicios.html';</script>";
+    echo "<script>alert('Registro eliminado correctamente'); location.href='../VISTA/principal/servicios.html';</script>";
 }else{
-    echo "<script>alert('Error al eliminar el registro.'); location.href='../VISTA/principal/servicios.html';</script>";
+    echo "<script>alert('Error al eliminar el registro'); location.href='../VISTA/principal/servicios.html';</script>";
 }
 
-mysqli_close($conectar);
 ?>
