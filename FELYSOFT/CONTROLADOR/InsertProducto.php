ff8bfd44fc38a4fc3a55febbe31579b9c3759860
MiguelC29 <miguelito28c@gmail.com>
2023-11-08 21:34:59 -0500

    Creacion de archivo PHP con insert de la tabla productos

---
+++ FELYSOFT/CONTROLADOR/InsertProducto.php
@@ -0,0 +1,23 @@
+<?php
+    require "Conexion.php";
+
+    //Recuperar las variables
+    $nomProduc = $_POST["nomP"];
+    $marca = $_POST["marca"];
+    $precioV = $_POST["precioV"];
+    $fVencimiento = $_POST["fVencimiento"];
+    $categoria = $_POST["categoria"];
+    $proveedor = $_POST["proveedor"];
+    $imagen = $_POST["formFile"];
+
+    //Creamos la sentencia de sql para insertar datos en la tabla productos
+    $insert = "INSERT INTO productos(nombre, marca, precioVenta, fechaVencimiento, fkIdCategoria, fkIdProveedor, imagen) VALUES ('$nomProduc', '$marca', '$precioV', '$fVencimiento', '$categoria', '$proveedor', '$imagen')";
+
+    //ejercutacion la sentencia de la linea 10
+    $query = mysqli_query($conectar, $insert);
+
+    //Verificar la conexión
+    if($query) {
+        echo "<script> alert('El producto $nombre se agregó correctamente.'); location.href='../VISTA/principal/productos.html'; </script>'";
+    }
+?>
