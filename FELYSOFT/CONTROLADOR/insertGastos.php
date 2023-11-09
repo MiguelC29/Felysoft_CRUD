f46b3e0b508ab96ac73ccc45c0ab6631384aca47
Pulgoso01 <tinitosguerrero@gmail.com>
2023-11-08 22:50:56 -0500

    Realizado mi trabajo

---
+++ FELYSOFT/CONTROLADOR/insertGastos.php
@@ -0,0 +1,20 @@
+<?php
+    require 'conexion.php';
+
+    //Recuperar las variables
+    $fecha = $_POST["fecha"];
+    $monto = $_POST["monto"];
+    $descripcion = $_POST["descripcion"];
+    $pago = $_POST["pago"];
+
+    
+    $insert = "INSERT INTO gastos(fecha, monto, descripcion, fkIdPago) VALUES ('$fecha', '$monto', '$descripcion', '$pago')";
+
+    //ejercutacion la sentencia de la linea 10
+    $query = mysqli_query($conectar, $insert);
+
+    //Verificar la conexión
+    if($query) {
+        echo "<script> alert('El gasto se agregó correctamente.'); location.href='../VISTA/principal/gastos.html'; </script>'";
+    }
+?>
