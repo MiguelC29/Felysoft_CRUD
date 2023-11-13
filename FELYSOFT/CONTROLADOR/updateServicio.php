6fd908f37891442435ac8364cbb6d6fe9e3942a9
devsan-g3 <santimahe001@gmail.com>
2023-11-13 13:32:32 -0600

    correcion link index.html desde servicios

--- FELYSOFT/CONTROLADOR/updateServicio.php
+++ FELYSOFT/CONTROLADOR/updateServicio.php
@@ -9,7 +9,7 @@
     <title>FELYSOFT</title>
     <link rel="shortcut icon" href="imagenes/icon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/Felysoft/FELYSOFT/VISTA/principal/style.css">
+    <link rel="stylesheet" href="/felysoft/FELYSOFT/VISTA/principal/style.css">
     <script src="scriptS.js"></script>
 </head>
 <body>
@@ -17,7 +17,7 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="sidebar">
-                    <a class="rounded" href="/prototipoFelysoft/principal/index.html">Venta</a>
+                    <a class="rounded" href="/felysoft/FELYSOFT/VISTA/principal/index.html">Venta</a>
                     <div class="dropdown">
                         <a class="rounded dropdown-toggle" href="#" role="button" id="dashboardDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                             Dashboard
@@ -125,9 +125,6 @@
                             <a href="/felysoft/FELYSOFT/VISTA/principal/servicios.php" id="cerrarFormulario" class="btn btn-danger">Cerrar</a>
                         </div>
                     </form>   
-                    
-                    
-                    
                         <?php
                             }
                         ?>
